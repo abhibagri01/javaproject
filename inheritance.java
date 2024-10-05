4806f627b82bdff61dcfce96db5a207dac9a5cdd
abhibagri01 <abhinavbagri174@gmail.com>
2024-10-05 19:14:00 +0530

    first

---
+++ inheritance.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+              //SINGLE INHERITANCE
+
+// class Animal{
+//     void eat() {System.out.println("eating...");}
+// }
+// class Dog extends Animal{
+// void bark(){System.out.println("barking...");}
+// }
+// class TestInheritance{
+//     public static void main(String arg[]){
+//         Dog d = new Dog();
+//         d.bark();
+//         d.eat();
+//     }
+// }
+
+                 // MULTILEVEL INHERITANCE
+
+// class Animal{
+//     void eat(){System.out.println("eating...");}
+// }  
+// class Dog extends Animal{
+//     void bark(){System.out.println("barking...");}
+// }               
+// class BabyDog extends Dog{
+// void weep(){System.out.println("weeping...");}
+// }
+// class TestInheritance{
+//     public static void main(String arg[]){
+// BabyDog d = new BabyDog();
+// d.weep();
+// d.bark();
+// d.eat();
+//     }
+// }
+
+ //HIERARCHICAL INHERITANCE
+
+        //   class Animal{
+        //         void eat(){System.out.println("eating...");}
+        //     }  
+        //     class Dog extends Animal{
+        //         void bark(){System.out.println("barking...");}   
+        //     }       
+        //     class Cat extends Dog{
+        //         void meow(){System.out.println("meowing...");}
+        //     }
+        //     class TestInheritance{
+        //         public static void main(String[] args) {
+        //         Cat c = new Cat();
+        //         c.meow();
+        //         c.eat();
+        //         }
+
+        //     }
+
+ 
---
+++ interface.java
@@ -0,0 +1,19 @@
+public class interface {
+    //interface decleration : by first user
+interface Drawable{
+       void draw();
+}
+// implementation : by second user
+class Rectangle implements Drawable{
+       public void draw(){System.out.println("drawing rectangle");}
+}
+class Circle implements Drawable{
+       public void draw(){System.out.println("drawing circle");}
+}
+//
+class TestInheritance{
+       public static void main(String[] args) {
+              Circle d = new Circle();
+              d.draw();
+       }
+}
