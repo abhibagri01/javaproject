876dde43f870b652b256cf272f5f398692f02a06
abhibagri01 <abhinavbagri174@gmail.com>
2024-11-07 18:50:12 +0530

    Add files via upload

---
+++ binomialCoff.java
@@ -0,0 +1,19 @@
+public class binomialCoff {
+        //BINOMIAL COEFFICENT
+
+public static int factorial(int n){
+        if(n==0) return 1;
+       return n*factorial(n-1);
+}
+public static int binCoeff(int n, int r){
+   int fact_n = factorial(n);
+   int fact_r = factorial(r);
+   int fact_nmr = factorial(n-r);
+    
+   int binCoeff = fact_n / (fact_r*fact_nmr);
+   return binCoeff;
+}
+public static void main(String[] args) {
+    System.out.println (binCoeff(5,2));
+}
+}
