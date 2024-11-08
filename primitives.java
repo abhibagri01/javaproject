8860d1c12b997875c318b97854711324a871d937
abhibagri01 <abhinavbagri174@gmail.com>
2024-11-08 19:08:00 +0530

    Add file via upload

---
+++ primitives.java
@@ -0,0 +1,19 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNo=65;
+        char a='4';
+        float h=65.32f;
+        double d=7367.2334;
+        long largeInt=293764943534L;
+        boolean flag = false;
+        System.out.println(rollNo);
+        System.out.println(a);
+        System.out.println(h);
+        System.out.println(d);
+        System.out.println(largeInt);
+        System.out.println(flag);
+        long num= 1_213_253_465_435_435L;
+        System.out.println(num);
+
+    }
+}
